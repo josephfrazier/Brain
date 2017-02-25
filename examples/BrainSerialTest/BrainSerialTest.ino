594fc33d8c1ec1c5d69758e6eebfb9bea976fc08
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-25 17:33:07 -0500

    wip: led

--- examples/BrainSerialTest/BrainSerialTest.ino
+++ examples/BrainSerialTest/BrainSerialTest.ino
@@ -9,11 +9,13 @@
 // Set up the brain parser, pass it the hardware serial object you want to listen on.
 Brain brain(Serial);
 
+const int led  =  8;     //use digital I/O pin 8
+
 void setup() {
     // Start the hardware serial.
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
-
+    pinMode(led,OUTPUT);   //set pin 8 to be an output output
 }
 
 void loop() {
@@ -22,10 +24,11 @@ void loop() {
     // "signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma"
     if (!brain.update()) {
         digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+        digitalWrite(led,LOW);    //set pin 8 LOW, turning off LED
+
         return;
     }
 
-    digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on
     Serial.println(brain.readErrors());
 
     char* str = brain.readCSV();
@@ -34,11 +37,24 @@ void loop() {
     // http://www.cplusplus.com/reference/cstring/strtok/
     char * pch;
     printf ("Splitting string \"%s\" into tokens:\n",str);
+    int i = 0;
     pch = strtok(str," ,.-");
     while (pch != NULL)
     {
+      if (i == 2) {
+        int val = atoi(pch);
+        if (0 < val && val < 40) {
+          digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on
+          digitalWrite(led,HIGH);   //set pin 8 HIGH, turning on LED
+        } else {
+          digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+          digitalWrite(led,LOW);    //set pin 8 LOW, turning off LED      
+        }
+      }
+      
       Serial.println(pch);
       pch = strtok(NULL, " ,.-");
+      i = i + 1;
     }
  
     delay(200);                       // wait for a bit
